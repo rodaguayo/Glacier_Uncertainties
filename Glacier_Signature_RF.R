3e70c4715156264fec96eb15e0b2edd9a4b40a86
Rodrigo Aguayo <roodaguayo@gmail.com>
2023-01-10 16:51:25 -0300

    delete unnecesary preprocess

--- Glacier_Signature_RF.R
+++ Glacier_Signature_RF.R
@@ -1,5 +1,5 @@
 # Code for XXXX ---------------------------------------------------------
-# Developed by Rodrigo Aguayo (2020-2022)
+# Developed by Rodrigo Aguayo (2022-2023)
 
 rm(list=ls())
 cat("\014")  
@@ -8,35 +8,23 @@ library("randomForest")
 setwd("/home/rooda/Dropbox/Patagonia/MS2 Results/")
 
 # data
-list_data <- list(Timing      = read.csv("data_peak_water_year.csv"),
-                  Rate_change = read.csv("data_rate_change.csv"),
-                  Duration    = read.csv("data_duration.csv"), 
-                  Magnitude   = read.csv("data_peak_water_value.csv"), 
-                  Frequency   = read.csv("data_freq.csv"))
-zones     <- c("CDI", "GCN", "NPI.E", "NPI.W", "PCA" ,  "PPY", "SPI.C", "SPI.N", "SPI.S")
-
 list_data <- list(Timing      = read.csv("data_basin_peak_water_year.csv"),
                   Rate_change = read.csv("data_basin_rate_change.csv"),
                   Duration    = read.csv("data_basin_duration.csv"), 
                   Magnitude   = read.csv("data_basin_peak_water_value.csv"), 
                   Frequency   = read.csv("data_basin_freq.csv"))
-for (i in 1:5){
+
+for (i in 1:5){ # at least one glacier in RGI6 and RGI7
   dataset <- list_data[[i]]
   dataset <- dataset[ , colSums(is.na(dataset)) == 0]
   list_data[[i]] <- dataset
 }
 
-for (i in 1:5){
-  dataset <- list_data[[i]]
-  list_data[[i]] <- dataset[,colnames(list_data$Rate_change)]
-}
-zones <- colnames(list_data$Rate_change)[8:164]
-
-
+basins <- colnames(list_data$Rate_change)[-7:0]
 variables <- c("Outline", "Climate", "Volume",  "GGM", "SSP", "BCM")
 rf_importance <- data.frame(matrix(ncol = 8, nrow = 0))
 
-for (i in 1:5){
+for (i in 1:5){ # for each metric
   
   # select dataset of glacier signature
   dataset <- list_data[[i]]
@@ -46,20 +34,20 @@ for (i in 1:5){
   dataset$Outline <- as.factor(dataset$Outline)
   dataset$Volume  <- as.factor(dataset$Volume)
   dataset$Climate <- as.factor(dataset$Climate)
-  dataset$GGM   <- as.factor(dataset$GGM)
-  dataset$SSP   <- as.factor(dataset$SSP)
-  dataset$BCM   <- as.factor(dataset$BCM)
+  dataset$GGM     <- as.factor(dataset$GGM)
+  dataset$SSP     <- as.factor(dataset$SSP)
+  dataset$BCM     <- as.factor(dataset$BCM)
   
-  for (j in 1:length(zones)){
+  for (j in 1:length(basins)){ # for each basin
     
     # select zone
-    zone       <- zones[j]
-    dataset_i  <- dataset[,c(variables, zone)]
+    basin       <- basins[j]
+    dataset_i  <- dataset[,c(variables, basin)]
     
     # random forest regression model
-    rf_model      <- randomForest(as.formula(paste0(zone, " ~.")), data = dataset_i, ntree = 500, importance = TRUE)
-    rf_importance <- rbind(rf_importance, c(zone, names(list_data)[i], importance(rf_model)[,1]))
-    print(i)
+    rf_model      <- randomForest(as.formula(paste0(basin, " ~.")), data = dataset_i, ntree = 500, importance = TRUE)
+    rf_importance <- rbind(rf_importance, c(basin, names(list_data)[i], importance(rf_model)[,1]))
+    print(paste0("Metric ", i," - ", "Basin ", j))
     }
 }
 
